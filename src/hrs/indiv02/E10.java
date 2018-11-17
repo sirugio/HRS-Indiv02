4109d0342d5927de78fdae3be82c300619f52c00
sirugio <sirugio@DESKTOP-TPOE4IB.home>
2018-11-17 18:22:18 +0100

    primer commit HRS-Indiv02

--- src/hrs/indiv02/E10.java
+++ src/hrs/indiv02/E10.java
@@ -1,4 +1,3 @@
-
 package hrs.indiv02;
 
 import java.util.Scanner;
@@ -13,12 +12,11 @@ public class E10 {
         numero = in.nextInt();
         if (numero > 0) {
             System.out.println("El número es positivo.");
-            for (i = numero - 1; i >= 1; i--) { 
+            for (i = numero - 1; i >= 1; i--) {
                 if (numero % i == 0) {
                     System.out.println("El número " + numero + " es divisible entre: " + i);
                 }
             }
-        } else 
-            System.out.println("Fin de programa.");       
+        }
     }
 }
